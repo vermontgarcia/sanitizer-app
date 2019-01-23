8da22adbbe2f669fc508b1a5c1809b6dc0413061
Vermont Garcia <vermontgarcia@hotmail.com>
2019-01-23 16:24:44 -0600

    Initial components

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {Button} from 'antd';
+
+class Home extends Component {
+	render(){
+		return (
+			<div>
+				<h1>Home</h1>
+				<Button type='primary'>Clik on me!</Button>
+
+			</div>
+		)
+	}
+}
+
+export default Home;
