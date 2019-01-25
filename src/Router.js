df6436ae907a9610fa0fcd15c4d69f7e9c90f1f4
Vermont Garcia <vermontgarcia@hotmail.com>
2019-01-24 20:34:17 -0600

    Signup route implemented

--- src/Router.js
+++ src/Router.js
@@ -4,16 +4,24 @@ import {Switch, Route} from 'react-router-dom';
 import Home from './Components/Home/Home';
 import NotFound from './404';
 import Login from './Components/Auth/Login';
+import Signup from './Components/Auth/Register';
 
 const Router = () => (
 	<Switch>
 		<Route exact path='/' render={() => (
 			<Home
-			/>)}
+			/>
+		)}
 		/>
 		<Route exact path='/login' render={() => (
 			<Login
-			/>)}
+			/>
+		)}
+		/>
+		<Route exact path='/signup' render={() => (
+			<Signup
+			/>
+		)}
 		/>
 		<Route path='*' component={NotFound} />
 	</Switch>
