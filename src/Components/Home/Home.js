0cbb94a130b6102dc9635de74a953658782bd139
Vermont Garcia <vermontgarcia@hotmail.com>
2019-01-29 19:31:35 -0600

    Token and session validation

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,7 +1,17 @@
 import React, {Component} from 'react';
 import {Button} from 'antd';
+import {isLoggedIn} from '../../authService';
 
 class Home extends Component {
+
+
+	componentWillMount(){
+
+    const token = localStorage.getItem('token');
+    token ? isLoggedIn(this.props.history) : this.props.history.push('/login');
+
+  }
+
 	render(){
 		return (
 			<div>
