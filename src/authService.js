1363ebb5908df2697f2b9acb0caea2e105012889
Vermont Garcia <vermontgarcia@hotmail.com>
2019-01-29 19:30:59 -0600

    Signup and login changes

--- src/authService.js
+++ src/authService.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 import {message} from 'antd';
 
-const base_url = window.location.hostname === 'localhost' ? 'http://localhost:3500/api' : 'https://sanitizer.herokuapp.com/api';
+const base_url = window.location.hostname === 'localhost' ? 'http://localhost:3500/api' : 'https://messagesanitizer.herokuapp.com/api';
 
 export const signup = (user, history) => {
 	axios.post(`${base_url}/auth/signup`, user)
 		.then(res => {
-			localStorage.setItem('token', res.data.token);
-			localStorage.setItem('user', JSON.stringify(res.data.user));
+			//localStorage.setItem('token', res.data.token);
+			//localStorage.setItem('user', JSON.stringify(res.data.user));
 			message.success(res.data.msg);
 			history.push('/')
 		})
@@ -20,6 +20,7 @@ export const login = (user, history) => {
 	axios.post(`${base_url}/auth/login`, user)
 		.then(res => {
 			localStorage.setItem('token', res.data.token);
+			localStorage.setItem('user', JSON.stringify(res.data.user));
 			message.success(res.data.msg);
 			history.push('/');
 		})
